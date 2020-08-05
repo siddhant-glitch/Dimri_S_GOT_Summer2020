af5c2b7562af826fab905fe5a54746ae1d057e15
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-08-05 03:53:48 -0400

    adding the fullscreen button and its function

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -5,10 +5,15 @@ console.log('fired!');
 let lightBox = document.querySelector(".lightbox"),
 	lbClose = lightBox.querySelector("span"),
 	lbVideo = lightBox.querySelector("video"),
-	sigils = document.querySelectorAll(".sigilContainer");
-	houseImages = document.querySelector("#houseImages");
-	houseName = document.querySelector(".house-name");
-	houseBio = document.querySelector(".house-info")
+	sigils = document.querySelectorAll(".sigilContainer"),
+	houseImages = document.querySelector("#houseImages"),
+	houseName = document.querySelector(".house-name"),
+	houseBio = document.querySelector(".house-info");
+
+	const fullscreenButton = document.querySelector('.back');
+	const fullscreenIcons = fullscreenButton.querySelectorAll('use');
+
+
 
 const houseInfo = [
 	["Stark",`House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`],
@@ -73,6 +78,18 @@ function animateBanner() {
 	//and
 }
 
+// toggleFullScreen toggles the full screen state of the video
+// If the browser is currently in fullscreen mode,
+// then it must be exited and vice versa.
+function toggleFullScreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    lbVideo.requestFullscreen();
+  }
+}
+
+
 // function hideLightbox() {
 // 	lightBox.classList.remove('show-lightbox');
 // }
@@ -82,6 +99,8 @@ function animateBanner() {
 sigils.forEach(sigil => sigil.addEventListener("click", animateBanner));
 
 lbClose.addEventListener("click", showHideLightbox);
+
+fullscreenButton.addEventListener("click", toggleFullScreen);
 
 // add an "ended" event handler for the video -> close the lightbox
 lbVideo.addEventListener("ended", showHideLightbox);
